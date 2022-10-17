1da3fe6f2651b629b64a563c1dbcb14d12db6b29
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-10-17 14:53:05 +0100

    leg segments

--- aircraft-initial-sizing/fuel_weight.py
+++ aircraft-initial-sizing/fuel_weight.py
@@ -4,3 +4,11 @@ class FuelWeight:
 
     def get_fuel_weight_fraction(self, total_weight: float) -> float:
         return total_weight * pow(0.97 * 0.985 * 0.995, 4.)
+
+class CruiseLeg:
+    def __init__(self, param):
+        self.name = param["name"]
+        self.range = param["range"]
+        self.altitude = param["altitude"]
+        self.mach_number = param["mach number"]
+        self.safety_factor = param["safety factor"]
